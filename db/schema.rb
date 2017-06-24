9066261f97acd38b7d9a6488a220c2271f00e148
mavyrick <jrsorokin@gmail.com>
2017-06-24 15:20:36 -0700

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170624192028) do
+ActiveRecord::Schema.define(version: 20170624212841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "notifications", force: :cascade do |t|
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_notifications_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -22,4 +31,5 @@ ActiveRecord::Schema.define(version: 20170624192028) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "notifications", "users"
 end
