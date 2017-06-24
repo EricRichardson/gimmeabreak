48df978c9679e490be368128867cc58a2970f969
Eric Richardson <eric@ericonrails.com>
2017-06-24 13:42:29 -0700

    Make create and show end point for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+  
+  def create
+    @user = User.new user_params
+    if @user.save
+      render json: @user
+    else
+      error = {error: 'Could not create user'}
+      render json: error
+    end
+  end
+
+  def show
+    @user = User.find params[:id]
+    respond_to do |format|
+      format.json { render json: @user }
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
